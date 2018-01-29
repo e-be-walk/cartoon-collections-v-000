a650eb78f5b99cade9c318d9c4a67d81b536b66f
Erin Walker <erinb0317@hotmail.com>
2018-01-29 23:46:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ end
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    puts "#{call.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
