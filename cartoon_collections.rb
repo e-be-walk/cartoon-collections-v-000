f6e2b1a8490616293299c8bee57b8770772bec9d
Erin Walker <erinb0317@hotmail.com>
2018-01-30 00:10:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,12 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  new_array = []
+  array.each do |word|
+    new_array << word.split("").count
+  end
+  new_array.any?{|i| i > 4}
 end
 
 def find_the_cheese(cheese_types)
